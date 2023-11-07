b6e5d9ee7c7ea6bf7077ae654ce545c009b7ac0f
Sean-Andrew-Stanek <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2023-11-07 21:35:03 +0900

    README.md updated for Exercise 4.1

--- src/App.js
+++ src/App.js
@@ -8,15 +8,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Here's a link to the repository:
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Sean-Andrew-Stanek/letsmeet"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LetsMeet GitHub Repository
         </a>
       </header>
     </div>
