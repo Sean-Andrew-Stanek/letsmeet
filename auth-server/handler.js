02dac00178dedb3afe9725c23d484c6b7ec444a2
Sean-Andrew-Stanek <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2023-11-09 23:48:38 +0900

    getAccessToken lambda added

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,3 +1,5 @@
+/* eslint-env node */
+
 'use strict';
 
 const {google} = require('googleapis');
@@ -5,7 +7,7 @@ const calendar = google.calendar('v3');
 const SCOPES = ['https://www.googleapis.com/auth/calendar.events.public.readonly'];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
 const redirect_uris = [
-    'https://sean-andrew-stanek.github.io'
+    'https://sean-andrew-stanek.github.io/letsmeet'
 ];
 
 const oAuth2Client = new google.auth.OAuth2(
@@ -30,4 +32,42 @@ module.exports.getAuthURL = async () => {
             authUrl,
         }),
     };
+};
+
+module.exports.getAccessToken = async (event) => {
+    //Decode from the URL query
+    const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+    return new Promise((resolve, reject) => {
+        /*
+        *   Exchange the AuthCode for the AccessToken with a callback after the exchange
+        *   either throwing a error or sending the response
+        */
+
+        oAuth2Client.getToken(code, (error, response) => {
+            if (error) {
+                console.log('oAuth2Client error');
+                return reject(error);
+            }
+            console.log('oAuth2Client successful');
+            return response;
+        });
+    }).then((results) => {
+        //Respond with the OAuth token
+        return {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+            },
+            body: JSON.stringify(results),
+        };
+    }).catch((error) => {
+        return {
+            statusCode: 500,
+            body: JSON.stringify(error),
+        };
+    });
+
+
 };
