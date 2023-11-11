e39f602f22d428ee6f4606750e431c918a554ebc
Sean-Andrew-Stanek <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2023-11-11 19:04:47 +0900

    Tests for CitySearch component

--- src/_tests_/CitySearch.test.js
+++ src/_tests_/CitySearch.test.js
@@ -1,1 +1,12 @@
 
+import {render} from '@testing-library/react';
+import CitySearch from '../components/CitySearch';
+
+describe('CitySearch component', () => {
+    test('Renders text input.', () => {
+        const CitySearchComponent = render(<CitySearch />);
+        const cityTextBox = CitySearchComponent.queryByRole('textbox');
+        expect(cityTextBox).toBeInTheDocument();
+        expect(cityTextBox).toHaveClass('city');
+    });
+});
