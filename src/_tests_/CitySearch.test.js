b1b1abad4ced1db0b2f0704d55e51887c02b583c
Sean-Andrew-Stanek <146111111+Sean-Andrew-Stanek@users.noreply.github.com>
2023-11-12 10:26:59 +0900

    CitySearchText => Comment consistency

--- src/_tests_/CitySearch.test.js
+++ src/_tests_/CitySearch.test.js
@@ -1,12 +1,40 @@
 
 import {render} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import CitySearch from '../components/CitySearch';
 
 describe('CitySearch component', () => {
+    
+    //AT START
+    //EXPECT COMP:  textbox
+    //WITH CLASS:   'city'
     test('Renders text input.', () => {
         const CitySearchComponent = render(<CitySearch />);
         const cityTextBox = CitySearchComponent.queryByRole('textbox');
         expect(cityTextBox).toBeInTheDocument();
         expect(cityTextBox).toHaveClass('city');
     });
+
+    //AT START
+    //UNEXPECT COMP:    List
+    test('Suggestion list is hidden by default', () => {
+        render(<CitySearch />);
+        const suggestionList = screen.queryByRole('list');
+        expect(suggestionList).not.toBeInTheDocument();
+    })
+
+    // WHEN USER:       Clicks textbox
+    // EXPECT COMP:     SuggestionList.js
+    // WITH CLASS:      'suggestions'
+    test('Renders a list of suggestions when the cityTextBox gains focus', async () => {
+        render(<CitySearch />);
+        const user = userEvent.setup();
+        await user.click(screen.queryByRole('textbox'));
+        const suggestionList = screen.queryByRole('list');
+        expect(suggestionList).toBeInTheDocument();
+        expect(suggestionList).toHaveClass('suggestions');
+    });
 });
+
+
+
